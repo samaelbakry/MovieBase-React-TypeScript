908b58bca3c67bdb040dd4cbada110538c4a6864
samaelbakry <samaabakry68@gmail.com>
2026-04-23 22:40:35 +0200

    feat: update import paths for utility functions to maintain consistency across components

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -3,9 +3,9 @@ import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react"
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
+import { cn } from "../../lib/utils"
+import { Button } from "./button"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react"
 import { Separator as SeparatorPrimitive } from "radix-ui"
+import { cn } from "../../lib/utils"
 
-import { cn } from "@/lib/utils"
 
 function Separator({
   className,
